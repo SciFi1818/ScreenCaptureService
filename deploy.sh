608728d5a1678df0772b2116bc371d1bee051db3
SciFi1818 <31384079+SciFi1818@users.noreply.github.com>
2018-03-10 14:01:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Upload travis generated APKs to the Transfer.sh
 cd /home/travis/build/SciFi1818/ScreenCaptureService/app/build/outputs/apk
 ls
-zip -r ScreenCaptureService.zip apk *
+zip ScreenCaptureService.zip  app-release-unsigned.apk
 cu=`curl --upload-file ScreenCaptureService.zip https://transfer.sh/ScreenCaptureService.zip`
 
 echo "Transfer.sh links:"
