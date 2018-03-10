2efc10c4844a2b89406b0b0a9d7163bd99ee4827
SciFi1818 <31384079+SciFi1818@users.noreply.github.com>
2018-03-10 13:45:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Upload travis generated APKs to the Transfer.sh
 cd /home/travis/build/SciFi1818/ScreenCaptureService/app/build/outputs/apk
 ls
-zip -r virtualhook.zip apk *
+zip -r ScreenCaptureService.zip apk *
 cu=`curl --upload-file ScreenCaptureService.zip https://transfer.sh/ScreenCaptureService.zip`
 
 echo "Transfer.sh links:"
